ce868f57442ed9ad898425ce95788d2d804519a8
Sender Audélia <senderaudelia@macbook-pro-de-sender.home>
2020-09-15 09:44:48 +0200

    validations set

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,9 @@
 class Video < ApplicationRecord
     belongs_to :playlist
+
+    validates :title, presence: true, length: { maximum: 30 }
+    validates :url, presence: true
+
+
+    #add validates :platform, presence: true
 end
