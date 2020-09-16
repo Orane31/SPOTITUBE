2f79f2292d79b76a524a4313a1d9b83e5a5f671f
Orane31 <orane.chaper@gmail.com>
2020-09-16 13:10:07 +0200

    user now has to choose his name when registering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+     
+    end
+
 
     def from_you?(id)
         if current_user.id == id
