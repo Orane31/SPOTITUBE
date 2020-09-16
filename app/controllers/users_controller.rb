59c424910b278fe5a6cab642a6ad554312c6d24e
Sender Audélia <senderaudelia@macbook-pro-de-sender.home>
2020-09-16 15:49:52 +0200

    our beautiful edit forms work soooo well

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,21 @@
 class UsersController < ApplicationController
 
-  def show 
-      @user = current_user
-  end
+    def show 
+        @user = current_user
+    end
 
-  def edit
-      @user = current_user
-  end
+    def edit
+        @user = current_user
+    end
 
-  def update
-      @user = current_user
-      @user.update(params.require(:user).permit(
-        :name, :description)
-      )
-      if @user.update 
-          flash[:success] = "Merci #{@user.name} ! Tes informations ont bien été modifiées."
-      else
-          flash[:danger] = "#{@user.errors.full_messages.each {|message| message}.join('')}"
-      end
-      redirect_to user_path(@user)
-  end
+    def update
+        @user = current_user
+        @user.update(params.require(:user).permit(:description))
+                
+        flash[:success] = "Merci #{@user.name} ! Tes informations ont bien été modifiées."
+
+        redirect_to user_path(@user)
+    end
 
 
 end
