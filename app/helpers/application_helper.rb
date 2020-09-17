d9d2833090938d65dac07bab071d6c87ebb7180e
Yoni19 <anthony.krief@hotmail.fr>
2020-09-17 19:51:10 +0200

    Alert fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     def bootstrap_class_for_flash(type)
         case type
           when 'notice' then "alert-info"
-          when 'success' then "alert-success"
+          when 'success' then "alert alert-success"
           when 'error' then "alert-danger"
           when 'alert' then "alert-warning"
         end
