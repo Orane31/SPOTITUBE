228e1b2e4d954451e95b810555116f1b3bc944ae
Sender Audélia <senderaudelia@macbook-pro-de-sender.home>
2020-09-18 02:32:56 +0200

    placing link icon + starting setting contact

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,22 @@
 class StaticPagesController < ApplicationController
 
-  def faq
-  end
+    def faq
+    end
 
-  def contact
-  end
+    def contact
+        if contact 
+            flash[:success] = "Merci #{current_user.name} ! Nous allons traiter ta demande dans les plus brefs délais"
+            redirect_to root_path
+        end
+    end
 
-  def team
-  end
+    def team
+    end
 
-  def about
-  end 
+    def about
+    end 
 
-  def error_404
-  end 
+    def error_404
+    end 
 
 end
